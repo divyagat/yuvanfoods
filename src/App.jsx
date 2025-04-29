fe866146c871fb06f1712cb65c14aaf0ef882fe5
yuvanfoods <yuvanfoods2309@gmail.com>
2025-04-29 18:23:01 +0530

    comite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-    <h1>hello</h1>
+    <h1 className='text-danger'>hello</h1>
     </>
   )
 }
