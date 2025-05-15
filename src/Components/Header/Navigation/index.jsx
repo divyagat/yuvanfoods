ab470b5996c736e607f945d44c7def13eb3177f7
yuvanfoods <yuvanfoods2309@gmail.com>
2025-05-15 12:50:10 +0530

    navbar

--- src/Components/Header/Navigation/index.jsx
+++ src/Components/Header/Navigation/index.jsx
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import { IoMdMenu } from "react-icons/io";
 import { TfiAngleDown } from "react-icons/tfi";
+import { Link } from 'react-router-dom';
 
 const Navigation = ()=>{
     return(
@@ -9,14 +10,22 @@ const Navigation = ()=>{
         <div className="container">
           <div className="row">
             <div className="col-sm-3 navPart1">
-              <button className='allCatTab align-items-center '>
+              <button className='allCatTab align-items-center mt-2'>
                 <span className='icon1'><IoMdMenu /></span>
                 <span className='text mx-2'>All CATEGORIES</span>
                 <span className='icon2'><TfiAngleDown /></span>
               </button>
             </div>
-            <div className="col-sm-9 navPart2">
-
+            <div className="col-sm-9 navPart2 d-flex align-items-center ">
+               <ul className=" list list-inline mx-auto">
+                <li className='list-inline-item'><Link to="/">Home</Link></li>
+                <li className='list-inline-item'><Link to="/">Fashion</Link></li>
+                <li className='list-inline-item'><Link to="/">Electronics</Link></li>
+                <li className='list-inline-item'><Link to="/">Bakery</Link></li>
+                <li className='list-inline-item'><Link to="/">Glocery</Link></li>
+                <li className='list-inline-item'><Link to="/">Blog</Link></li>
+                <li className='list-inline-item'><Link to="/">Contact Us</Link></li>
+               </ul>
             </div>
           </div>
         </div>
